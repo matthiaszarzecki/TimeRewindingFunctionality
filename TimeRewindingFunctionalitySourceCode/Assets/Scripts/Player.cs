81f756cf88597ac8faee00c811472ea25c4723ed
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2016-04-20 15:58:08 +0200

    Sourcecode commit

---
+++ TimeRewindingFunctionalitySourceCode/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player: MonoBehaviour
+{
+    private TimeController timeController;
+
+    void Start()
+    {
+        timeController = FindObjectOfType(typeof(TimeController)) as TimeController;
+    }
+	
+	void Update()
+	{
+        if(!timeController.isReversing)
+        {
+		    transform.Translate (Vector3.forward * 3.0f * Time.deltaTime * Input.GetAxis ("Vertical"));
+		    transform.Rotate (Vector3.up * 200.0f * Time.deltaTime * Input.GetAxis ("Horizontal"));
+        }
+	}
+}
---
+++ TimeRewindingFunctionalitySourceCode/Assets/Scripts/TimeController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController: MonoBehaviour
+{
+	public GameObject player;
+    public ArrayList playerPositions;
+    public ArrayList playerRotations;
+    public bool isReversing = false;
+
+	void Start()
+	{
+        playerPositions = new ArrayList();
+        playerRotations = new ArrayList();
+	}
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            isReversing = true;
+        }
+        else
+        {
+            isReversing = false;
+        }
+    }
+	
+	void FixedUpdate()
+	{
+        if(!isReversing)
+        {
+            playerPositions.Add (player.transform.position);
+            playerRotations.Add (player.transform.localEulerAngles);
+        }
+        else
+        {
+            player.transform.position = (Vector3) playerPositions[playerPositions.Count - 1];
+            playerPositions.RemoveAt(playerPositions.Count - 1);
+
+            player.transform.localEulerAngles = (Vector3) playerRotations[playerRotations.Count - 1];
+            playerRotations.RemoveAt(playerRotations.Count - 1);
+        }
+	}
+}
